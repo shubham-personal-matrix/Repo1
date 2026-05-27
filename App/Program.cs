997f726520bcbe770ddd7ba7cc9feed6472610c2
shubham.bansal <shubham.bansal@matrixcomsec.com>
2026-05-27 17:50:28 +0530

    changes method name as per common repo

--- App/Program.cs
+++ App/Program.cs
@@ -2,7 +2,7 @@
 
 DeviceHelper helper = new DeviceHelper();
 
-Console.WriteLine(helper.GetDeviceStatus());
+Console.WriteLine(helper.GetDeviceStatusTemp());
 Console.WriteLine(helper.GetDeviceIP());
 
 Console.ReadLine();
